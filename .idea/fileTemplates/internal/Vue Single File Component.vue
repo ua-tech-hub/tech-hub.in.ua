8538efad81d7974b64d0f06f929703932b3c930e
Alex Kozack <cawa-93@users.noreply.github.com>
2022-05-06 23:04:46 +0300

    Інтеграція з nuxt

---
+++ .idea/fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: ${DIR_PATH}/${FILE_NAME}
+  </div>
+</template>
+
+<style scoped></style>
---
+++ app.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup></script>
+
+<template>
+  <Html lang="uk"></Html>
+
+  <Head>
+    <Title>
+      🇺🇦 Хаб — Сайт в розробці
+    </Title>
+  </Head>
+
+  <under-construction-message/>
+</template>
+
+<style>
+html {
+  font-size: 26px;
+  text-align: center;
+  margin: 0;
+  min-height: 100vh;
+  display: flex;
+  font-family: -apple-system,
+  "Segoe UI",
+  Roboto,
+  "Helvetica Neue", Arial,
+  "Noto Sans",
+  "Liberation Sans",
+  sans-serif,
+  "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+}
+
+body {
+  max-width: 30rem;
+  padding: 2rem;
+  margin: auto;
+}
+</style>
